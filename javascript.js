f1f213f305e95aade444c665cb2aae5916a1a3ed
zalec <142818531+z4le3c@users.noreply.github.com>
2023-09-19 11:09:27 -0600

    feat: add getComputerChoice and playSingleRound

---
+++ javascript.js
@@ -0,0 +1,46 @@
+// randomly returns 'Rock', 'Paper' or 'Scissors'
+function getComputerChoice() {
+    rnumber = Math.floor(Math.random() * 3)
+    let choice;
+    switch (rnumber) {
+        case 0:
+            choice = 'Rock';
+            break;
+        case 1:
+            choice = 'Scissors';
+            break;
+        case 2:
+            choice = 'Paper';
+            break;
+    }
+
+    return choice;
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+    function serialize(choice) {
+        choice = choice.toLowerCase();
+        switch (choice) {
+            case 'rock':
+                return 0;
+            case 'scissors':
+                return 1;
+            case 'paper':
+                return 2;
+        }
+    }
+
+    let nPlayerChoice = serialize(playerSelection);
+    let nComputerChoice = serialize(computerSelection);
+
+    if (nPlayerChoice == nComputerChoice) {
+        console.log('Is a tie!')
+    } else if ((nPlayerChoice + 1) % 3 == nComputerChoice) {
+        console.log(`You win!, ${playerSelection} beats ${computerSelection}`)
+    } else {
+        console.log(`You lose!, ${computerSelection} beats ${playerSelection}`)
+    }
+}
+
+
+console.log(playSingleRound('scissors', 'rock'))
